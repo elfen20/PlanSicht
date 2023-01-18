7c58fd41fae516dddb5acd4ff6a82831a738ae41
Elfen20 <elfen20@outlook.de>
2023-01-18 16:14:05 +0100

    + Redraw, Zoom, Translate

--- PlanSicht/MainForm.Designer.cs
+++ PlanSicht/MainForm.Designer.cs
@@ -29,28 +29,34 @@ namespace PlanSicht
         /// </summary>
         private void InitializeComponent()
         {
-            this.pView = new System.Windows.Forms.Panel();
             this.pSide = new System.Windows.Forms.Panel();
+            this.tbStatus = new System.Windows.Forms.TextBox();
             this.bOpen = new System.Windows.Forms.Button();
+            this.bResetZoom = new System.Windows.Forms.Button();
+            this.bResetPos = new System.Windows.Forms.Button();
             this.pSide.SuspendLayout();
             this.SuspendLayout();
             // 
-            // pView
-            // 
-            this.pView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pView.Location = new System.Drawing.Point(0, 0);
-            this.pView.Name = "pView";
-            this.pView.Size = new System.Drawing.Size(1007, 642);
-            this.pView.TabIndex = 0;
-            // 
             // pSide
             // 
+            this.pSide.Controls.Add(this.tbStatus);
+            this.pSide.Controls.Add(this.bResetPos);
+            this.pSide.Controls.Add(this.bResetZoom);
             this.pSide.Controls.Add(this.bOpen);
             this.pSide.Dock = System.Windows.Forms.DockStyle.Right;
-            this.pSide.Location = new System.Drawing.Point(926, 0);
+            this.pSide.Location = new System.Drawing.Point(807, 0);
             this.pSide.Name = "pSide";
-            this.pSide.Size = new System.Drawing.Size(81, 642);
+            this.pSide.Size = new System.Drawing.Size(200, 642);
             this.pSide.TabIndex = 0;
+            // 
+            // tbStatus
+            // 
+            this.tbStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbStatus.Location = new System.Drawing.Point(3, 479);
+            this.tbStatus.Multiline = true;
+            this.tbStatus.Name = "tbStatus";
+            this.tbStatus.Size = new System.Drawing.Size(194, 160);
+            this.tbStatus.TabIndex = 1;
             // 
             // bOpen
             // 
@@ -60,6 +66,27 @@ namespace PlanSicht
             this.bOpen.TabIndex = 0;
             this.bOpen.Text = "Open";
             this.bOpen.UseVisualStyleBackColor = true;
+            this.bOpen.Click += new System.EventHandler(this.bOpen_Click);
+            // 
+            // bResetZoom
+            // 
+            this.bResetZoom.Location = new System.Drawing.Point(3, 32);
+            this.bResetZoom.Name = "bResetZoom";
+            this.bResetZoom.Size = new System.Drawing.Size(75, 23);
+            this.bResetZoom.TabIndex = 0;
+            this.bResetZoom.Text = "Reset Zoom";
+            this.bResetZoom.UseVisualStyleBackColor = true;
+            this.bResetZoom.Click += new System.EventHandler(this.bResetZoom_Click);
+            // 
+            // bResetPos
+            // 
+            this.bResetPos.Location = new System.Drawing.Point(3, 61);
+            this.bResetPos.Name = "bResetPos";
+            this.bResetPos.Size = new System.Drawing.Size(75, 23);
+            this.bResetPos.TabIndex = 0;
+            this.bResetPos.Text = "Reset Pos";
+            this.bResetPos.UseVisualStyleBackColor = true;
+            this.bResetPos.Click += new System.EventHandler(this.bResetPos_Click);
             // 
             // MainForm
             // 
@@ -67,19 +94,24 @@ namespace PlanSicht
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1007, 642);
             this.Controls.Add(this.pSide);
-            this.Controls.Add(this.pView);
+            this.DoubleBuffered = true;
             this.Name = "MainForm";
             this.Text = "PlanSicht";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseUp);
             this.pSide.ResumeLayout(false);
+            this.pSide.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel pView;
         private System.Windows.Forms.Panel pSide;
         private System.Windows.Forms.Button bOpen;
+        private System.Windows.Forms.TextBox tbStatus;
+        private System.Windows.Forms.Button bResetPos;
+        private System.Windows.Forms.Button bResetZoom;
     }
 }
 
--- PlanSicht/MainForm.cs
+++ PlanSicht/MainForm.cs
@@ -12,18 +12,134 @@ namespace PlanSicht
 {
     public partial class MainForm : Form
     {
+        private Bitmap PlanImage;
+        private PointF PlanViewTranslation = Point.Empty;
+        private Point MouseStartPos = Point.Empty;
+        private Point MouseDelta = Point.Empty;
+        private int PlanViewZoom = 0;
+        private float Zoomfactor = 1f;
+
         public MainForm()
         {
-            InitializeComponent();
-            splitContainer1.Panel1.Paint += Panel1_Paint;
+            InitializeComponent();            
+            this.Paint += pView_Paint;            
+            this.MouseWheel += MainForm_MouseWheel;
         }
 
-        private void Panel1_Paint(object sender, PaintEventArgs e)
+        private void Redraw()
         {
-            using (var g = e.Graphics)
+            this.Invalidate();
+            this.Update();
+            showStatus();
+        }
+
+        private void MainForm_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta > 0)
+                PlanViewZoom++;
+            else
+                PlanViewZoom--;
+            Zoomfactor = (float)Math.Pow(2d, PlanViewZoom / 5d);
+            Redraw();
+        }
+
+        private void showStatus()
+        {
+            var b = new StringBuilder();
+            if (PlanImage != null)
             {
-                g.FillRectangle(Brushes.Red, new Rectangle(10, 10, 20, 20));
+                b.AppendLine($"W: {PlanImage.Width}");
+                b.AppendLine($"W: {PlanImage.Height}");
             }
+            else b.AppendLine("No Image");
+            b.AppendLine($"T: {PlanViewTranslation}");
+            b.AppendLine($"Z: {Zoomfactor}");
+            tbStatus.Text = b.ToString();
+        }
+
+        private void pView_Paint(object sender, PaintEventArgs e)
+        {
+            if (PlanImage != null && ((PlanImage.Width>0 && PlanImage.Height>0)))
+            {
+                var g = e.Graphics;
+                {
+                    RectangleF source = new RectangleF(0, 0, PlanImage.Width, PlanImage.Height);
+                    float wz = PlanImage.Width * Zoomfactor;
+                    float hz = PlanImage.Height * Zoomfactor;
+                    int mx = pSide.Left / 2;
+                    int my = pSide.Height / 2;
+
+                    RectangleF dest = new RectangleF(MouseDelta.X + PlanViewTranslation.X - wz / 2 + mx, MouseDelta.Y + PlanViewTranslation.Y - hz / 2 + my, wz, hz);
+                    g.DrawImage(PlanImage, dest, source, GraphicsUnit.Pixel);
+                }
+            }
+        }
+
+        private void pView_MouseUp(object sender, MouseEventArgs e)
+        {
+            Redraw();
+        }
+
+        private void DrawonBitmap(Bitmap b)
+        {
+            using (var g = Graphics.FromImage(b))
+            {
+                g.Clear(Color.DarkGray);
+                using (var p = new Pen(Brushes.Red))
+                {                   
+                    p.Width = 2;
+                    g.DrawEllipse(p, 0, 0, b.Width-1, b.Height-1);
+                }
+            }
+        }
+
+        private void bOpen_Click(object sender, EventArgs e)
+        {           
+            PlanImage?.Dispose();
+            PlanImage = new Bitmap(1000, 1000);
+            DrawonBitmap(PlanImage);
+            Redraw();
+        }
+
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                MouseStartPos = e.Location;
+            }
+        }
+
+        private void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                MouseDelta = new Point(e.X - MouseStartPos.X, e.Y - MouseStartPos.Y);
+                Redraw();
+            }
+        }
+
+        private void MainForm_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                PlanViewTranslation.X += MouseDelta.X;
+                PlanViewTranslation.Y += MouseDelta.Y;
+                MouseDelta = Point.Empty;
+                Redraw();
+            }
+        }
+
+        private void bResetZoom_Click(object sender, EventArgs e)
+        {
+            PlanViewZoom = 0;
+            Zoomfactor = 1f;
+            Redraw();
+        }
+
+        private void bResetPos_Click(object sender, EventArgs e)
+        {
+            PlanViewTranslation = PointF.Empty;
+            Redraw();
         }
     }
 }
